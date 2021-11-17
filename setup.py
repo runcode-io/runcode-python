74059f45b06aa7da158cdc846e0bc6140a72cfa4
Ashwin <ashwin@micropyramid.com>
2021-11-17 19:15:47 +0530

    v0.2

--- setup.py
+++ setup.py
@@ -2,38 +2,37 @@ import io
 import os
 from setuptools import setup, find_packages
 
-VERSION = '0.1'
+VERSION = "0.2"
 
-with open(os.path.join(os.path.dirname(__file__), 'README.md')) as readme:
+with open(os.path.join(os.path.dirname(__file__), "README.md")) as readme:
     README = readme.read()
 
 
 setup(
-    name='runcode',
+    name="runcode",
     version=VERSION,
-    author='Micro Pyramid Informatic Pvt. Ltd.',
-    author_email='hello@micropyramid.com',
-    url='https://github.com/runcode-io/runcode-python',
-    description='RunCode Python API',
+    author="RunCode",
+    author_email="hello@runcode.io",
+    url="https://github.com/runcode-io/runcode-python",
+    description="RunCode Python API",
     long_description=README,
-    long_description_content_type='text/markdown',
-    packages=find_packages(exclude=['tests', 'tests.*']),
+    long_description_content_type="text/markdown",
+    packages=find_packages(exclude=["tests", "tests.*"]),
     include_package_data=True,
-    install_requires=[
-        'requests'
-    ],
+    install_requires=["requests"],
     classifiers=[
-        'Intended Audience :: Developers',
-        'Operating System :: OS Independent',
-        'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Topic :: Software Development :: Internationalization',
+        "Intended Audience :: Developers",
+        "Operating System :: OS Independent",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Topic :: Software Development :: Internationalization",
     ],
 )
